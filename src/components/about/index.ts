68d9efaa0059ea5528075984e0675b2a98394c94
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-27 09:32:20 +0500

    reexports about sections

---
+++ src/components/about/index.ts
@@ -0,0 +1,6 @@
+export { Info } from "./info";
+export { Theme } from "./theme";
+export { Program } from "./program";
+export { Time } from "./time";
+export { Place } from "./place";
+export { Bron } from "./bron";
