b5e4f9a3e4cd0cfc706ac8def8de33169a4cf9d2
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-27 14:23:57 +0500

    for laptop adaptive layout

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -2,12 +2,14 @@ import { FC } from "react";
 
 export const HomeHero: FC = () => {
   return (
-    <section className="flex flex-col gap-5 h-[600px]">
-      <div className="embla">
-        <div className="embla__container">
-          <div className="embla__slide h-[600px] object-cover">
-            <img src="/banner.png" alt="" className="size-full object-cover" />
-          </div>
+    <section className="embla">
+      <div className="embla__container">
+        <div className="embla__slide">
+          <img
+            src="/banner.png"
+            alt="banner"
+            className="size-full object-cover max-h-[600px] min-h-[420px]"
+          />
         </div>
       </div>
     </section>
