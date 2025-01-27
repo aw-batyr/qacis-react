71ae99a21306ec3119f6a599432d8ab5ed3e696a
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-27 17:11:54 +0500

    added loading and fetching news

--- src/components/home/news.tsx
+++ src/components/home/news.tsx
@@ -1,31 +1,44 @@
 import { cn } from "@/lib/utils";
 import { FC } from "react";
 import { Container } from "../layout";
-import { NewsCard } from "../shared";
+import { Loading, NewsCard } from "../shared";
 import { Button } from "../ui";
+import { useNews } from "@/services/hooks/use-news";
+import { Link } from "react-router-dom";
 
 interface Props {
   className?: string;
 }
 
 export const News: FC<Props> = ({ className }) => {
+  const { data, isPending } = useNews();
+
+  console.log(data);
+
   return (
     <section className={cn("bg-[url('/geo-bg.png')] py-16", className)}>
       <Container className="flex flex-col gap-8">
         <h3 className="text-3xl text-on_primary">Новости</h3>
 
         <div className="grid grid-cols-3 gap-6">
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
+          {isPending ? (
+            <Loading />
+          ) : (
+            data
+              ?.slice(0, 3)
+              .map((item) => (
+                <NewsCard
+                  className="!bg-[#E0E6EB]"
+                  {...item}
+                  key={item.title}
+                />
+              ))
+          )}
         </div>
 
-        <Button variant={"outline"} className="w-fit mx-auto">
-          Все новости
-        </Button>
+        <Link to="news" className="w-fit mx-auto">
+          <Button variant={"outline"}>Все новости</Button>
+        </Link>
       </Container>
     </section>
   );
