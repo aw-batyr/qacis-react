d36a7ab3dce8715c45067c599b7754969dcb3cfb
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-27 17:14:04 +0500

    services: news and news-inner

---
+++ src/services/index.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { NewsDatum, NewsInnerType, NewsType } from "./types/news";
+
+const URL = "https://qacis.turkmenexpo.com/app/api/v1";
+
+export const getNews = async () => {
+  const data = axios.get<NewsType>(`${URL}/news`);
+
+  return data;
+};
+
+export const getNewsInner = async (id: number) => {
+  const data = axios.get<NewsInnerType>(`${URL}/news/${id}`);
+
+  return data;
+};
