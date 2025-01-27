f48d21ea784a72732e511ad9415be221efcac2a2
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-27 17:13:51 +0500

    news page ready

---
+++ src/pages/news.tsx
@@ -0,0 +1,24 @@
+import { Container } from "@/components/layout";
+import { Loading, NewsCard } from "@/components/shared";
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { useNews } from "@/services/hooks/use-news";
+import { FC } from "react";
+
+export const News: FC = () => {
+  useScrollTop();
+  const { data, isPending } = useNews();
+
+  if (isPending) return <Loading />;
+
+  return (
+    <Container className="pt-20 pb-[120px]">
+      <h1 className="text-center text-5xl mb-10">Новости</h1>
+
+      <div className="grid grid-cols-3 gap-6">
+        {data?.map((item) => (
+          <NewsCard {...item} key={item.title} />
+        ))}
+      </div>
+    </Container>
+  );
+};
