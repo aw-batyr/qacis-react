427f2925f9f4974ca382d54fac5db61a1bd9577b
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-27 09:42:24 +0500

    removed letter

--- src/lib/get-b2b-form-details.ts
+++ src/lib/get-b2b-form-details.ts
@@ -91,7 +91,7 @@ export const defaultValuesOfB2b = {
   job_title: "",
   participants_number: "",
   country: "",
-  email_address: "a",
+  email_address: "",
   phone_number: "",
   website: "",
   meeting_objective: "",
