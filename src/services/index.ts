b6b1362c453dfc5588dd07c13ee76dadc6dde08f
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-27 17:15:18 +0500

    build

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { NewsDatum, NewsInnerType, NewsType } from "./types/news";
+import { NewsInnerType, NewsType } from "./types/news";
 
 const URL = "https://qacis.turkmenexpo.com/app/api/v1";
 
