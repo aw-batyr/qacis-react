f88f707c74080399e26caee5ed8e4d3593181a55
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-27 17:12:16 +0500

    added two buttons

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,6 +4,7 @@ import { Container } from "./";
 import { Link } from "react-router-dom";
 import { navData } from "@/data";
 import { LangMenu } from "../shared";
+import { Button } from "../ui";
 
 interface Props {
   className?: string;
@@ -13,7 +14,7 @@ export const Header: FC<Props> = ({ className }) => {
   return (
     <header className={cn("h-[92px] bg-primary_10 py-2", className)}>
       <Container className="flex items-center justify-between">
-        <div className="flex-[0_0_681px] flex items-center justify-between">
+        <div className="flex items-center gap-10">
           <Link to="/">
             <img src="/logo.svg" alt="logo" />
           </Link>
@@ -27,7 +28,27 @@ export const Header: FC<Props> = ({ className }) => {
           </div>
         </div>
 
-        <LangMenu />
+        <div className="flex items-center gap-10">
+          <div className="flex items-center gap-6">
+            <Link to={"/B2B-B2G"}>
+              <Button
+                size={"sm"}
+                className="bg-reverse_primary text-on_primary_container hover:bg-reverse_primary/90 "
+              >
+                B2B | B2G встречи
+              </Button>
+            </Link>
+            <Link to={"/B2B-B2G"}>
+              <Button
+                size={"sm"}
+                className="bg-reverse_primary text-on_primary_container  hover:bg-reverse_primary/90  "
+              >
+                Официальная поддержка
+              </Button>
+            </Link>
+          </div>
+          <LangMenu />
+        </div>
       </Container>
     </header>
   );
