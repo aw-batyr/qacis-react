295cabba36d4641e89c4f5a399e11cef77503cbd
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-27 17:12:54 +0500

    fetching data for news card

--- src/components/shared/news-card.tsx
+++ src/components/shared/news-card.tsx
@@ -1,42 +1,49 @@
 import { cn } from "@/lib/utils";
 import { FC } from "react";
 import { ArrowRight } from "lucide-react";
+import { FeaturedImage } from "@/services/types/news";
+import { Link } from "react-router-dom";
 
 interface Props {
+  id: number;
   date?: string;
   title?: string;
-  img?: string;
+  featured_images?: FeaturedImage[];
   className?: string;
 }
 
-export const NewsCard: FC<Props> = ({ className }) => {
+export const NewsCard: FC<Props> = ({
+  className,
+  featured_images,
+  title,
+  id,
+}) => {
   return (
-    <article
-      className={cn("bg-surface_container flex flex-col gap-6", className)}
-    >
-      <div className="flex flex-col gap-4 px-6 pt-6">
-        <div className="flex items-center justify-between">
-          <div>
-            <span>17</span>
-            <span>января</span>
+    <Link to={`/news/${id}`}>
+      <article
+        className={cn("bg-surface_container flex flex-col gap-6", className)}
+      >
+        <div className="flex flex-col gap-4 px-6 pt-6">
+          <div className="flex items-center justify-between">
+            <div>
+              <span>17 </span>
+              <span>января</span>
+            </div>
+
+            <ArrowRight />
           </div>
 
-          <ArrowRight />
+          <hr />
+
+          <h3 className="text-[20px] leading-[130%] line-clamp-3">{title} </h3>
         </div>
 
-        <hr />
-
-        <h3 className="text-[20px] leading-[130%]">
-          В Ашхабаде прошла встреча Рашида Мередова с помощником министра
-          иностранных дел КНР
-        </h3>
-      </div>
-
-      <img
-        src="/news-img.png"
-        alt="news image"
-        className="size-full object-cover"
-      />
-    </article>
+        <img
+          src={featured_images?.[0]?.path || ""}
+          alt="news image"
+          className="w-full h-[220px] object-cover"
+        />
+      </article>
+    </Link>
   );
 };
