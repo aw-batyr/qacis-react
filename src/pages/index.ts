bc428165c3595387ad1a508c831827b6a47586e0
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-27 17:13:29 +0500

    news and news-inner reexport

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,5 @@ export { Home } from "./home";
 export { B2b } from "./b2b";
 export { Delegate } from "./delegate";
 export { About } from "./about";
+export { News } from "./news";
+export { NewsInner } from "./news-inner";
