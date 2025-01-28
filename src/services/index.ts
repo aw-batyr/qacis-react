bcd0c57aa95e3fcc98a0aa785d0a7ee1589ba23c
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-28 21:04:09 +0500

    added post delegate

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { NewsInnerType, NewsType } from "./types/news";
+import { DelegateFormType } from "@/lib/get-delegate-form-details";
 
 const URL = "https://qacis.turkmenexpo.com/app/api/v1";
 
@@ -13,4 +14,24 @@ export const getNewsInner = async (id: number) => {
   const data = axios.get<NewsInnerType>(`${URL}/news/${id}`);
 
   return data;
+};
+
+export const postDelegate = async (
+  data: DelegateFormType
+): Promise<boolean> => {
+  const res = axios.post(`${URL}/become_delegate`, data);
+
+  return (await res).status === 201;
+};
+
+export const postB2b = async (data: FormData): Promise<boolean> => {
+  const res = axios.post(`${URL}/form`, data);
+
+  return (await res).status === 201;
+};
+
+export const postSponsor = async (data: DelegateFormType): Promise<boolean> => {
+  const res = axios.post(`${URL}/become_sponsor`, data);
+
+  return (await res).status === 201;
 };
