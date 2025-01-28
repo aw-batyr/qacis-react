ed9f46c618a0ca8a88f6cfe83e959f864dca6928
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-28 21:59:42 +0500

    added loading isPending

--- src/components/home/home-news.tsx
+++ src/components/home/home-news.tsx
@@ -14,32 +14,31 @@ export const News: FC<Props> = ({ className }) => {
   const { data, isPending } = useNews();
 
   return (
-    <section className={cn("bg-[url('/geo-bg.png')] py-16", className)}>
-      <Container className="flex flex-col gap-8">
-        <h3 className="text-3xl text-on_primary">Новости</h3>
+    <section
+      className={cn(
+        "bg-[url('/geo-bg.png')] bg-no-repeat bg-cover py-16",
+        className
+      )}
+    >
+      {isPending ? (
+        <Loading />
+      ) : (
+        <Container className="flex flex-col gap-8">
+          <h3 className="text-3xl text-on_primary">Новости</h3>
 
-        <div className="grid md:grid-cols-3 grid-cols-1 gap-6">
-          {isPending ? (
-            <Loading />
-          ) : (
-            data
-              ?.slice(0, 3)
-              .map((item) => (
-                <NewsCard
-                  className="!bg-[#E0E6EB]"
-                  {...item}
-                  key={item.title}
-                />
-              ))
-          )}
-        </div>
+          <div className="grid md:grid-cols-3 grid-cols-1 gap-6">
+            {data?.slice(0, 3).map((item) => (
+              <NewsCard className="!bg-[#E0E6EB]" {...item} key={item.title} />
+            ))}
+          </div>
 
-        <Link to="news" className="md:w-fit w-full mx-auto">
-          <Button variant={"outline"} className="w-full">
-            Все новости
-          </Button>
-        </Link>
-      </Container>
+          <Link to="news" className="md:w-fit w-full mx-auto">
+            <Button variant={"outline"} className="w-full">
+              Все новости
+            </Button>
+          </Link>
+        </Container>
+      )}
     </section>
   );
 };
