dbfc225da06dc2eef7cd26301321b762abf7665d
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-28 13:20:55 +0500

    lg, md, sm banners added

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,14 +1,24 @@
 import { FC } from "react";
+import { useMediaQuery } from "usehooks-ts";
 
 export const HomeHero: FC = () => {
+  const lg = useMediaQuery("(min-width: 1024px)");
+  const md = useMediaQuery("(min-width: 768px)");
+
+  function getBanner() {
+    if (lg) return "/banners/ru/l.jpg";
+    else if (md) return "/banners/ru/m.jpg";
+    else return "/banners/ru/s.jpg";
+  }
+
   return (
     <section className="embla">
       <div className="embla__container">
         <div className="embla__slide">
           <img
-            src="/banner.png"
+            src={getBanner()}
             alt="banner"
-            className="size-full object-cover md:max-h-[600px] md:min-h-[420px] max-h-[420px] min-h-[355px]"
+            className="size-full object-cover lg:max-h-[600px] "
           />
         </div>
       </div>
