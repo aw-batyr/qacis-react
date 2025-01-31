c65bd41f80306eacd83e21ee879f85122b4b3853
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-01-31 15:33:29 +0500

    close icon scale size changed

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -62,8 +62,8 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
-      <SheetPrimitive.Close className="absolute right-4 p-3 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-        <X className="size-6 text-white" />
+      <SheetPrimitive.Close className="absolute right-4 p-3 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-secondary">
+        <X className="size-8 text-white" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
       {children}
