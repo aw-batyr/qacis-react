65038f0d6fb7619b782e519173fe6216a17ac948
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-02-03 14:07:16 +0500

    added link and btnText

--- src/components/home/home-offers.tsx
+++ src/components/home/home-offers.tsx
@@ -38,11 +38,14 @@ export const HomeOffers: FC = () => {
               img="/offer-1.png"
               className="embla__slide flex-[0_0_300px] sm:flex-[0_0_600px]"
               title="Гостиницы, трансфер, визы"
+              btnText="Скачать путеводитель"
               link="https://qacis.turkmenexpo.com/app/storage/app/media/travel_guide/Travel_guide_ru.pdf"
               text="По вопросам размещения в гостиницах, визовой поддержки, транспортного и экскурсионного обслуживания Вы можете обращаться к официальным туроператорам выставки"
             />
             <OfferCard
+              link="https://qacis.turkmenexpo.com/app/storage/app/media/floor%20plan/Floor%20plan.pdf"
               img="/offer-2.png"
+              btnText="Скачать план выставки"
               className="embla__slide flex-[0_0_300px] sm:flex-[0_0_600px]"
               title="Ознакомьтесь с планом выставки ITSE 2025"
               text="Как выбрать лучшее место на выставке? Вы всегда должны помнить, что удачное расположение выставочной экспозиции повышает Ваши шансы привлечь внимание Ваших потенциальных клиентов"
