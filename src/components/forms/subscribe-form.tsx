95d01d80dde1a19094d1c2bc60ba697ad1fef8b1
Batyr <awbatyr@gmail.com>
2025-02-06 15:23:43 +0500

    footer form is ready

---
+++ src/components/forms/subscribe-form.tsx
@@ -0,0 +1,59 @@
+import { FC } from "react";
+import { Container } from "../layout";
+import { useLang } from "@/hooks/use-lang";
+import { Button } from "../ui";
+import { Field } from "../shared";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+interface Props {
+  className?: string;
+}
+
+const schema = z.object({
+  email: z.string().email(),
+});
+
+export type SubscribeType = z.infer<typeof schema>;
+
+export const SubscribeForm: FC<Props> = () => {
+  const form = useForm<SubscribeType>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  async function onSubmit(data: SubscribeType) {
+    console.log(data);
+  }
+
+  return (
+    <form
+      onSubmit={form.handleSubmit(onSubmit)}
+      className="bg-surface_container py-8"
+    >
+      <Container className="flex items-center justify-between">
+        <h2 className="h2">
+          {useLang("Подпишитесь на новости:", "Subscribe to the news:")}
+        </h2>
+
+        <div className="relative">
+          <input
+            {...form.register("email")}
+            placeholder="E-mail"
+            className="input xl:w-[392px] w-[320px]"
+          />
+          <span className="text-error absolute -bottom-6 text-sm left-0">
+            {form.formState.errors?.email?.message}
+          </span>
+        </div>
+
+        <Button className="xl:w-[288px] w-[220px]">
+          {useLang("Подписаться", "Subscribe")}
+        </Button>
+      </Container>
+    </form>
+  );
+};
