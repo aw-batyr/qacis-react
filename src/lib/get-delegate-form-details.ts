e11ab1d4dcaa5e74ba38eab4523065eb54a99b8d
Kakabay <2kakabayashyrberdyew@gmail.com>
2025-02-21 12:53:39 +0500

    added become sponsor field

--- src/lib/get-delegate-form-details.ts
+++ src/lib/get-delegate-form-details.ts
@@ -29,6 +29,8 @@ export const delegateFormSchema = z.object({
 
   become_speaker: z.boolean(),
 
+  become_sponsor: z.boolean(),
+
   visa_support: z.string().optional(),
 });
 
@@ -41,5 +43,6 @@ export const delegateDefaultValues = {
   phone: "",
   website: "",
   become_speaker: false,
+  become_sponsor: false,
   visa_support: "",
 };
