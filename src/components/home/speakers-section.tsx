e9c967d5b84777135dedf523fb1dc2df6a06211e
Batyr <awbatyr@gmail.com>
2025-03-10 16:46:11 +0500

    renamed

---
+++ src/components/home/speakers-section.tsx
@@ -0,0 +1,42 @@
+import { cn } from "@/lib/utils";
+import { FC } from "react";
+import { Container } from "../layout";
+import { Loading, SpeakerCard } from "../shared";
+import { Button } from "../ui";
+import { useSpeakers } from "@/services/hooks/use-speakers";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+
+interface Props {
+  className?: string;
+  section?: boolean;
+}
+
+export const SpeakersSection: FC<Props> = ({ className, section = false }) => {
+  const { data, isPending } = useSpeakers();
+
+  const { t } = useTranslation("home");
+
+  if (isPending) return <Loading />;
+
+  return (
+    <section className={cn("my-20", className)}>
+      <Container className="flex flex-col gap-6">
+        {section && <h2 className="text-3xl">{t("speakers.title")}</h2>}
+        <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
+          {data?.map((item, i) => (
+            <SpeakerCard key={i} {...item} />
+          ))}
+        </div>
+
+        {section && (
+          <Link to="/speakers" className="mx-auto">
+            <Button variant="outline" className="text-on_surface">
+              {t("speakers.btn")}
+            </Button>
+          </Link>
+        )}
+      </Container>
+    </section>
+  );
+};
