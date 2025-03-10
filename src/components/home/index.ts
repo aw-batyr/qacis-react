2ea8db87ff7a4b5e6f00ea3cf31f229a88c35809
Batyr <awbatyr@gmail.com>
2025-03-10 16:46:03 +0500

    added speakers page

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -2,7 +2,7 @@ export { Contacts } from "./contacts";
 export { Partners } from "./partners";
 export { HomeHero } from "./hero";
 export { HomeAbout } from "./home-about";
-export { Speakers } from "./speakers";
+export { SpeakersSection } from "./speakers-section";
 export { Sponsors } from "./sponsors";
 export { News } from "./home-news";
 export { HomeOffers } from "./home-offers";
