fbf52459c0ff5f77d16a9d701021b3e79f221b0a
Batyr <awbatyr@gmail.com>
2025-03-10 16:48:50 +0500

    removed any stroke of code

--- src/pages/speakers.tsx
+++ src/pages/speakers.tsx
@@ -3,8 +3,6 @@ import { cn } from "@/lib/utils";
 import { Cover } from "@/components/layout";
 import { useTranslation } from "react-i18next";
 import { SpeakersSection } from "@/components/home/speakers-section";
-import { useSpeakers } from "@/services/hooks/use-speakers";
-import { Loading } from "@/components/shared";
 
 interface Props {
   className?: string;
@@ -12,10 +10,6 @@ interface Props {
 
 export const Speakers: FC<Props> = ({ className }) => {
   const { t } = useTranslation("home");
-
-  const { data, isPending } = useSpeakers();
-
-  if (isPending) return <Loading />;
 
   return (
     <div className={cn("", className)}>
