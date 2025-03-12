40632f83063cbf735aecad3a6bd90f6d7a05fbeb
Batyr <awbatyr@gmail.com>
2025-03-12 15:30:41 +0500

    added useScrollTop

--- src/pages/speakers.tsx
+++ src/pages/speakers.tsx
@@ -3,12 +3,14 @@ import { cn } from "@/lib/utils";
 import { Cover } from "@/components/layout";
 import { useTranslation } from "react-i18next";
 import { SpeakersSection } from "@/components/home/speakers-section";
+import { useScrollTop } from "@/hooks/use-scroll-top";
 
 interface Props {
   className?: string;
 }
 
 export const Speakers: FC<Props> = ({ className }) => {
+  useScrollTop();
   const { t } = useTranslation("home");
 
   return (
