5a844b24e0f09ac98ea0bfe9c09e786052dde650
Batyr <awbatyr@gmail.com>
2025-03-25 18:39:20 +0500

    added faq navigation

--- src/components/shared/dropdown.tsx
+++ src/components/shared/dropdown.tsx
@@ -12,18 +12,18 @@ export const Dropdown: FC<Props> = ({ className, question, answer }) => {
   const [active, setActive] = useState(false);
 
   return (
-    <div className={cn("", className)}>
+    <div className={cn("rounded-[8px]", className)}>
       <div
         onClick={() => setActive(!active)}
-        className="flex items-center cursor-pointer bg-surface_container gap-6 justify-between  p-6"
+        className="flex items-center cursor-pointer bg-surface_container  justify-between p-6"
       >
         <h2 className="md:text-lg">{question}</h2>
 
         <div className="size-12 flex items-center justify-center relative">
-          <span className="bg-on_surface w-3 h-0.5 rounded" />
+          <span className="bg-on_surface w-3 h-0.5 rounded rotate-180" />
           <motion.span
             initial={{ opacity: 0 }}
-            animate={active ? { opacity: 1 } : {}}
+            animate={!active ? { opacity: 1 } : {}}
             className="absolute top-1/2 left-1/2 -translate-x-1/2 rounded -translate-y-1/2 bg-on_surface w-3 h-0.5 rotate-90"
           />
         </div>
@@ -33,23 +33,28 @@ export const Dropdown: FC<Props> = ({ className, question, answer }) => {
         initial={{
           height: 0,
           opacity: 0,
+          paddingBottom: 0,
           paddingTop: 0,
           pointerEvents: "none",
+          marginBottom: 0,
         }}
         animate={
           active
             ? {
-                height: "100%",
+                height: "fit-content",
                 opacity: 1,
                 paddingTop: 24,
+                paddingBottom: 24,
                 pointerEvents: "all",
+                marginBottom: 24,
               }
             : {}
         }
-        className="bg-[#E0E6EB] p-6"
+        transition={{ type: "tween", duration: 0.2 }}
+        className="bg-[#E0E6EB] px-6 overflow-hidden"
       >
         <div
-          className="news-inner"
+          className="news-inner text-base"
           dangerouslySetInnerHTML={{ __html: answer ?? "" }}
         />
       </motion.div>
--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { cn } from "@/lib/utils";
 import { Container, Cover } from "@/components/layout";
-import { Dropdown } from "@/components/shared";
+import { Dropdown, Loading } from "@/components/shared";
 import { useFaq } from "@/services/hooks/use-faq";
 
 interface Props {
@@ -9,23 +9,31 @@ interface Props {
 }
 
 export const Faq: FC<Props> = ({ className }) => {
-  const { data } = useFaq();
+  const { data, isPending } = useFaq();
+
+  if (isPending) return <Loading />;
 
   return (
     <div className={cn("", className)}>
       <Cover title="FAQ" />
 
-      <Container className="md:mt-20 mt-10 md:mb-[120px] mb-20 flex flex-col gap-10 md:gap-[120px]">
-        {data?.map((item) => (
-          <div className="">
-            <h2 className="h2 mb-6">{item?.name}</h2>
+      {isPending ? (
+        <Loading />
+      ) : (
+        <Container className="md:mt-20 mt-10 md:mb-[120px] mb-20 gap-10 flex flex-col">
+          {data?.map((item) => (
+            <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
+              <h2 className="h2">{item?.name}</h2>
 
-            {item?.faqs.map((faq) => (
-              <Dropdown {...faq} />
-            ))}
-          </div>
-        ))}
-      </Container>
+              <div className="flex flex-col gap-4 md:gap-6">
+                {item?.faqs.map((faq) => (
+                  <Dropdown key={faq?.answer} {...faq} />
+                ))}
+              </div>
+            </div>
+          ))}
+        </Container>
+      )}
     </div>
   );
 };
