d2f594c259d3a54b81e6a4b735c32dd396dcabf1
Batyr <awbatyr@gmail.com>
2025-05-06 14:21:37 +0500

    commit

--- src/services/hooks/use-photos.ts
+++ src/services/hooks/use-photos.ts
@@ -1,13 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import { useLangStore } from "@/store/lang";
 import { getPhotos } from "../services";
 
 export const usePhotos = (id: number) => {
-  const lang = useLangStore((state) => state.lang);
-
   const { data, isPending } = useQuery({
-    queryKey: ["photos", lang, id],
-    queryFn: () => getPhotos(lang, id),
+    queryKey: ["photos", id],
+    queryFn: () => getPhotos(id),
     select: ({ data }) => data.data,
   });
 
